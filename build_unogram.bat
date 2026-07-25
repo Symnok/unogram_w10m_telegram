93f51ca97f3457bc96b527ebe89739b3f5d2b1b4
nalion <nallion.xiaomi@gmail.com>
2026-07-26 01:13:14 +0200

    rUIRi7b0T5_nallion

--- build_unogram.bat
+++ build_unogram.bat
@@ -1,3 +1,6 @@
+rmdir /s C:\UNOGRAM_UWP
+git clone https://github.com/nallion/tdlib_wp10.git C:\UNOGRAM_UWP
+cd C:\UNOGRAM_UWP
 nuget restore TelegramWP10.sln
 msbuild TelegramWP10.sln /p:Configuration=Release /v:m /p:Platform=ARM
 pause
