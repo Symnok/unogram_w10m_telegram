c0d3854bccb479f45f619f3cd974cd226db6d56f
nalion <nallion.xiaomi@gmail.com>
2026-07-28 09:17:56 +0200

    Disable keyboard hiding between message inputs (2)

--- commit.sh
+++ commit.sh
@@ -6,5 +6,5 @@ RANDOM_MSG=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)
 echo $RANDOM_MSG
 git add -A
 #git commit -m "$RANDOM_MSG"_nallion
-git commit -m "Disable keyboard hiding between message inputs"
+git commit -m "Disable keyboard hiding between message inputs (2)"
 git push
