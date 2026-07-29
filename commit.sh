880fb4c149599547381ff1b899ea10caa1ac69c2
nalion <nallion.xiaomi@gmail.com>
2026-07-29 13:52:43 +0200

    Update ChatsCounter when chat is deleted

--- commit.sh
+++ commit.sh
@@ -6,5 +6,5 @@ RANDOM_MSG=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)
 echo $RANDOM_MSG
 git add -A
 #git commit -m "$RANDOM_MSG"_nallion
-git commit -m "Remove debug logging..."
+git commit -m "Update ChatsCounter when chat is deleted"
 git push
